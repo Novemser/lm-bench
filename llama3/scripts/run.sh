e5ef849d18ddee8b6ef808ab99effcd85b61f714
yicw <weggleyi@gmail.com>
2024-04-29 16:13:06 +0800

    add script to cal similarity for weight importance

--- llama3/scripts/run.sh
+++ llama3/scripts/run.sh
@@ -1,7 +1,16 @@
 source ./scripts/config.sh
 
-tasks="copa"
-./scripts/eval.sh
+# tasks="copa"
+# ./scripts/eval.sh
 
-tasks="lambada_openai"
+# tasks="lambada_openai"
+# ./scripts/eval.sh
+
+# tasks="piqa"
+# ./scripts/eval.sh
+
+# tasks="mmlu"
+# ./scripts/eval.sh
+
+tasks="gsm8k"
 ./scripts/eval.sh
